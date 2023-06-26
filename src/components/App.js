9751569f319cf83b1ff4e7664d4ae210cee42ad0
Manish <121147194+Manish68829@users.noreply.github.com>
2023-06-26 13:49:57 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Sentence from "./Sentence";
 
 const App = () => {
-  return (
-    <div id="main"></div>
+  return ( 
+    <div id="main">fdsfdsf
+      <Sentence></Sentence>
+    </div>
   )
 }
 
---
+++ src/components/Sentence.js
@@ -0,0 +1,12 @@
+import react, {useState} from "react";
+
+function Sentence(){
+
+    return(
+        <div id="root">
+         <p>"I am learning React. My life is getting better."</p>
+        </div>
+    )
+}
+
+export default Sentence;
